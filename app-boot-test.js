5006d55c41a1542bb3b9572ba22ac92b8da34020
Patrick Johnson <patrick.johnson3@gmail.com>
2026-07-02 06:15:17 -0400

    Extract fake DOM test helpers

--- app-boot-test.js
+++ app-boot-test.js
@@ -1,108 +1,5 @@
 import assert from "node:assert/strict";
-import { domIds } from "./dom-ids.js";
-
-class FakeClassList {
-  constructor() {
-    this.classes = new Set();
-  }
-
-  add(...classes) {
-    classes.forEach((className) => this.classes.add(className));
-  }
-
-  contains(className) {
-    return this.classes.has(className);
-  }
-
-  remove(...classes) {
-    classes.forEach((className) => this.classes.delete(className));
-  }
-
-  toggle(className, force) {
-    const enabled = force === undefined ? !this.classes.has(className) : force;
-    if (enabled) this.classes.add(className);
-    else this.classes.delete(className);
-    return enabled;
-  }
-}
-
-class FakeStyle {
-  constructor() {
-    this.properties = {};
-  }
-
-  setProperty(name, value) {
-    this.properties[name] = value;
-  }
-}
-
-class FakeElement {
-  constructor(id = "") {
-    this.id = id;
-    this.children = [];
-    this.childNodes = this.children;
-    this.classList = new FakeClassList();
-    this.listeners = [];
-    this.style = new FakeStyle();
-    this.attributes = {};
-    this.offsetHeight = id === "marble" ? 58 : 0;
-    this.offsetWidth = id === "marble" ? 58 : 0;
-    this.textContent = "";
-  }
-
-  addEventListener(type, listener, options) {
-    this.listeners.push({ type, listener, options });
-  }
-
-  appendChild(child) {
-    this.children.push(child);
-    this.childNodes = this.children;
-    return child;
-  }
-
-  replaceChildren(...children) {
-    this.children = children;
-    this.childNodes = this.children;
-  }
-
-  setAttribute(name, value) {
-    this.attributes[name] = String(value);
-  }
-}
-
-function createDocument() {
-  const elements = Object.fromEntries(
-    Object.values(domIds).map((id) => [id, new FakeElement(id)])
-  );
-  const labelSpans = new Map(
-    [
-      "speedSetting",
-      "sensitivitySetting",
-      "rotationSetting",
-      "hapticsSetting",
-      "trailSetting",
-      "fullscreenSetting"
-    ].map((id) => [id, new FakeElement()])
-  );
-
-  return {
-    body: new FakeElement("body"),
-    documentElement: new FakeElement("html"),
-    title: "",
-    visibilityState: "visible",
-    addEventListener() {},
-    createElement() {
-      return new FakeElement();
-    },
-    getElementById(id) {
-      return elements[id] || null;
-    },
-    querySelector(selector) {
-      const match = selector.match(/^label\[for="([^"]+)"\] span$/);
-      return match ? labelSpans.get(match[1]) || null : null;
-    }
-  };
-}
+import { createFakeDocument } from "./test-dom.js";
 
 const originalGlobals = {
   addEventListener: globalThis.addEventListener,
@@ -116,7 +13,7 @@ const originalGlobals = {
   window: globalThis.window
 };
 
-const document = createDocument();
+const document = createFakeDocument();
 globalThis.addEventListener = () => {};
 globalThis.document = document;
 globalThis.innerHeight = 844;
---
+++ test-dom.js
@@ -0,0 +1,104 @@
+import { domIds } from "./dom-ids.js";
+
+export class FakeClassList {
+  constructor() {
+    this.classes = new Set();
+  }
+
+  add(...classes) {
+    classes.forEach((className) => this.classes.add(className));
+  }
+
+  contains(className) {
+    return this.classes.has(className);
+  }
+
+  remove(...classes) {
+    classes.forEach((className) => this.classes.delete(className));
+  }
+
+  toggle(className, force) {
+    const enabled = force === undefined ? !this.classes.has(className) : force;
+    if (enabled) this.classes.add(className);
+    else this.classes.delete(className);
+    return enabled;
+  }
+}
+
+export class FakeStyle {
+  constructor() {
+    this.properties = {};
+  }
+
+  setProperty(name, value) {
+    this.properties[name] = value;
+  }
+}
+
+export class FakeElement {
+  constructor(id = "") {
+    this.id = id;
+    this.children = [];
+    this.childNodes = this.children;
+    this.classList = new FakeClassList();
+    this.listeners = [];
+    this.style = new FakeStyle();
+    this.attributes = {};
+    this.offsetHeight = id === "marble" ? 58 : 0;
+    this.offsetWidth = id === "marble" ? 58 : 0;
+    this.textContent = "";
+  }
+
+  addEventListener(type, listener, options) {
+    this.listeners.push({ type, listener, options });
+  }
+
+  appendChild(child) {
+    this.children.push(child);
+    this.childNodes = this.children;
+    return child;
+  }
+
+  replaceChildren(...children) {
+    this.children = children;
+    this.childNodes = this.children;
+  }
+
+  setAttribute(name, value) {
+    this.attributes[name] = String(value);
+  }
+}
+
+export function createFakeDocument() {
+  const elements = Object.fromEntries(
+    Object.values(domIds).map((id) => [id, new FakeElement(id)])
+  );
+  const labelSpans = new Map(
+    [
+      "speedSetting",
+      "sensitivitySetting",
+      "rotationSetting",
+      "hapticsSetting",
+      "trailSetting",
+      "fullscreenSetting"
+    ].map((id) => [id, new FakeElement()])
+  );
+
+  return {
+    body: new FakeElement("body"),
+    documentElement: new FakeElement("html"),
+    title: "",
+    visibilityState: "visible",
+    addEventListener() {},
+    createElement() {
+      return new FakeElement();
+    },
+    getElementById(id) {
+      return elements[id] || null;
+    },
+    querySelector(selector) {
+      const match = selector.match(/^label\[for="([^"]+)"\] span$/);
+      return match ? labelSpans.get(match[1]) || null : null;
+    }
+  };
+}
